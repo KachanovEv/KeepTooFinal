a3ee1cd85fd5bf8ba501aaae48f3d229cdd4eaf8
KachanovEv <kachanov.ua@gmail.com>
2019-04-01 13:58:57 +0300

    16

--- app/src/main/java/com/example/keeptoofinal/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/keeptoofinal/adapter/RecyclerViewAdapter.java
@@ -5,10 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 import com.example.keeptoofinal.R;
 import com.example.keeptoofinal.model.Data;
-
 
 import org.jetbrains.annotations.NotNull;
 
@@ -19,35 +17,43 @@ import androidx.recyclerview.widget.RecyclerView;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     private ArrayList<Data> itemsData;
 
-    public RecyclerViewAdapter(ArrayList<Data> itemsData) {
+    public void setData(ArrayList<Data> itemsData) {
+        this.itemsData = itemsData;
+        notifyDataSetChanged();
+
+
+    }
+
+
+    public RecyclerViewAdapter() {
         this.itemsData = itemsData;
     }
 
 
     @NotNull
     @Override
-    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NotNull ViewGroup parent,
                                                              int viewType) {
         View itemLayoutView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.layout_item, null);
+                .inflate(R.layout.layout_item, parent, false);
 
         return new ViewHolder(itemLayoutView);
     }
 
     @Override
     public void onBindViewHolder(@NotNull ViewHolder viewHolder, int position) {
-
-
+//        Glide.with(this.context).load(itemsData.get(position).imageId).into(viewHolder.media_image);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView textView1;
-        public ImageView media_image;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView media_image;
 
         ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
-
+            media_image = itemLayoutView.findViewById(R.id.media_image);
         }
     }
 
